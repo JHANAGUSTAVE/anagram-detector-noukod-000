0098808f1e4c00abb951f733a639a962682a0540
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-06 15:14:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,15 +6,7 @@ class Anagram
     @word_to_analize = word_to_analize
   end
   
-  def match(find_anagram_in_this_string) 
-    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
-    find_anagram_in_this_string.find_all do |word| 
-      if word.split("").sort == self.word_to_analyze.split("").sort 
-        word 
-      end
-    end 
-  end
-  
+ 
   listen = Anagram.new("listen")
 listen.match(%w(enlists google inlets inlest banana))
   
